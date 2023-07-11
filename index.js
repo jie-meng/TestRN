d22f260670ad2372239ff98138bb1f8984eb4ce7
Steve Cheng <biao.cheng@thoughtworks.com>
2023-07-11 17:36:41 +0800

    add code from voice call sample app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 import '@azure/core-asynciterator-polyfill';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './src/components/App';
+import {name as appName} from './src/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
