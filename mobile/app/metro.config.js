8476d9261c325a6d2b46c418acc7cd045178a83d
Steve Cheng <biao.cheng@thoughtworks.com>
2023-07-19 16:53:19 +0800

    use monorepo

---
+++ mobile/app/metro.config.js
@@ -0,0 +1,40 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+const fs = require('fs');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+const monorepoPackages = path.resolve(workspaceRoot, 'mobile'); // uncomment this line after adding packages and add it in watch folders
+const projectNodeModules = path.resolve(projectRoot, 'node_modules');
+const nodeModulesPaths = [path.resolve(workspaceRoot, 'node_modules')];
+
+if (fs.existsSync(projectNodeModules)) {
+  nodeModulesPaths.push(projectNodeModules);
+}
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: {
+      ...require('node-libs-react-native'),
+      net: require.resolve('node-libs-react-native/mock/net'),
+      tls: require.resolve('node-libs-react-native/mock/tls'),
+    },
+  },
+  watchFolders: [__dirname, ...nodeModulesPaths, monorepoPackages],
+};
+
+module.exports = config;
